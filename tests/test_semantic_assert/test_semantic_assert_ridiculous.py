ba2e5f00d991f288cb6665f844b9dd897976c0e7
Shredmetal <morganj.lee01@gmail.com>
2024-11-16 20:23:39 +0800

    added ridiculous emoji test case

--- tests/test_semantic_assert/test_semantic_assert_ridiculous.py
+++ tests/test_semantic_assert/test_semantic_assert_ridiculous.py
@@ -16,3 +16,11 @@ class TestComplexSemanticAssertion:
         actual = "🤖" * 100
         expected = "A lot of emojis"
         asserter.assert_semantic_match(actual, expected)
+
+    def test_expect_few_but_get_a_ton_of_emojis(self, asserter):
+        """Test recognition of a pile of meaningless emojis"""
+        actual = "🤖" * 100
+        expected = "Only a few emojis"
+        with pytest.raises(SemanticAssertionError) as excinfo:
+            asserter.assert_semantic_match(actual, expected)
+        assert "Semantic assertion failed" in str(excinfo.value)
