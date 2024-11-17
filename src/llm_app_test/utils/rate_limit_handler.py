3496f95c9b2ba5df0fe4652b765f029b89e3d02c
Shredmetal <morganj.lee01@gmail.com>
2024-11-17 18:35:06 +0800

    we need a rate limit handler. stick it in utils maybe find a better place for it later idk

---
+++ src/llm_app_test/utils/rate_limit_handler.py
@@ -0,0 +1,5 @@
+# Solution 1: Go to Anthropic with a rubber hose and employ said hose until they increase rate limits
+
+# Solution 2: Implement a rate limit handler.
+
+# Conclusion: Solution 2 is less likely to invite liability, therefore solution 2 is the correct approach. Get coding!
