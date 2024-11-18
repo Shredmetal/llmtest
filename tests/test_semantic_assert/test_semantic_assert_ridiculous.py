bf25c25dcdeca9df880c3e72733ede17ae2d274a
Shredmetal <morganj.lee01@gmail.com>
2024-11-18 21:52:53 +0800

    edge case problem - again, being weird

--- tests/test_semantic_assert/test_semantic_assert_ridiculous.py
+++ tests/test_semantic_assert/test_semantic_assert_ridiculous.py
@@ -28,7 +28,7 @@ class TestRidiculousSemanticAssertion:
     def test_unicode_mess(self, asserter):
         """Test handling of unicode chaos"""
         actual = "🤖👾" * 50 + "こんにちは" * 20 + "🌈" * 30
-        expected = "A bunch of emojis and Japanese text"
+        expected = "More than one type of emoji and Japanese text regardless of order"
         asserter.assert_semantic_match(actual, expected)
 
     def test_mixed_language_emoji_spam(self, asserter):
