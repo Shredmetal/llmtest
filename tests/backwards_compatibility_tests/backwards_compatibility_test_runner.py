290250b0328d7eb9ea63e5dccbe9c43725942669
Morgan <morganj.lee01@gmail.com>
2024-12-01 21:58:37 +0800

    Added env var testing suites to the test runners for both deprecated and current paths

--- tests/backwards_compatibility_tests/backwards_compatibility_test_runner.py
+++ tests/backwards_compatibility_tests/backwards_compatibility_test_runner.py
@@ -17,6 +17,7 @@ def run_semantic_tests():
         str(test_dir / "test_semantic_assert" / "test_semantic_assert_configuration.py"),
         str(test_dir / "test_semantic_assert" / "test_semantic_assert_prompt_configurator.py"),
         str(test_dir / "test_semantic_assert" / "test_semantic_assert_llm_injection.py"),
+        str(test_dir / "test_semantic_assert" / "test_semantic_assert_env_configuration.py"),
         str(test_dir / "actual_usage_tests" / "test_greeting.py"),
         str(test_dir / "actual_usage_tests" / "test_ww2_narrative.py"),
         str(test_dir / "test_semantic_assert" / "test_semantic_assert_basic.py"),
